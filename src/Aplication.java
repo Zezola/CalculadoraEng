0d3470f13411dee8f59724dad31c216b941efb56
Ronald <ronald.jc@hotmail.com>
2021-04-12 15:40:29 -0300

    Feito a classe que realiza o somatorio

--- src/Aplication.java
+++ src/Aplication.java
@@ -5,5 +5,26 @@ public class Aplication {
 		
 		Scanner in = new Scanner(System.in);
 		
+		 short opcao = 0;
+		
+		System.out.println("Escolha uma das opções abaixo");
+		System.out.println("1- Somar");
+		System.out.println("2- Subtrair");
+		System.out.println("3- Multiplicar");
+		System.out.println("4- Dividir");
+		opcao = in.nextShort();
+		
+		switch(opcao) {
+		case 1:
+			double num1,  num2;
+			Somar result = new Somar();
+				System.out.print("Primeiro valor: ");
+				num1 = in.nextDouble();
+				System.out.print("Segundo valor: ");
+				num2 = in.nextDouble();
+				
+				System.out.println(result.somatorio(num1, num2));
+			break;
+		}
 	}
 }
---
+++ src/Somar.java
@@ -0,0 +1,12 @@
+
+public class Somar {
+	
+	public double somatorio(double num1, double num2) {
+		double result;
+		
+		result = num1 + num2;
+		
+		return result;
+	}
+
+}
