4475a43e74e5e3ab8e1baf361185a18b6b7b4c83
Ronald <ronald.jc@hotmail.com>
2021-04-13 09:18:51 -0300

    Foi adicionado as 4 operacoes basicas

--- src/Aplication.java
+++ src/Aplication.java
@@ -2,32 +2,83 @@ import java.util.*;
 
 public class Aplication {
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
-		
-		 short opcao = 0;
-		
+
+		short opcao = 0;
+		double num1 = 0, num2 = 0;
+
 		System.out.println("Escolha uma das opções abaixo");
 		System.out.println("1- Somar");
 		System.out.println("2- Subtrair");
 		System.out.println("3- Multiplicar");
 		System.out.println("4- Dividir");
+		System.out.println("5- Resto da divisão");
 		opcao = in.nextShort();
-		
-		switch(opcao) {
-		
+
+		switch (opcao) {
+
 		case 1:
-			
-			double num1,  num2;
-			Somar result = new Somar();
-				System.out.print("Primeiro valor: ");
-				num1 = in.nextDouble();
-				System.out.print("Segundo valor: ");
-				num2 = in.nextDouble();
-				
-				System.out.println(result.somatorio(num1, num2));
-				
+
+			Calcula somar = new Calcula();
+			System.out.print("Primeiro valor: ");
+			num1 = in.nextDouble();
+			System.out.print("Segundo valor: ");
+			num2 = in.nextDouble();
+
+			somar.somar(num1, num2);
+
 			break;
+
+		case 2:
+
+			Calcula subtracao = new Calcula();
+			System.out.print("Primeiro valor: ");
+			num1 = in.nextDouble();
+			System.out.print("Segundo valor: ");
+			num2 = in.nextDouble();
+
+			subtracao.subtrair(num1, num2);
+
+			break;
+
+		case 3:
+
+			Calcula multiplica = new Calcula();
+			System.out.print("Primeiro valor: ");
+			num1 = in.nextDouble();
+			System.out.print("Segundo valor: ");
+			num2 = in.nextDouble();
+
+			multiplica.multiplicar(num1, num2);
+
+			break;
+
+		case 4:
+
+			Calcula dividir = new Calcula();
+			System.out.print("Primeiro valor: ");
+			num1 = in.nextDouble();
+			System.out.print("Segundo valor: ");
+			num2 = in.nextDouble();
+
+			dividir.divisao(num1, num2);
+
+			break;
+
+		case 5:
+
+			Calcula restoDivisao = new Calcula();
+			System.out.print("Primeiro valor: ");
+			num1 = in.nextDouble();
+			System.out.print("Segundo valor: ");
+			num2 = in.nextDouble();
+
+			restoDivisao.restoDivisao(num1, num2);
+
+			break;
+
 		}
+
 	}
 }
---
+++ src/Calcula.java
@@ -0,0 +1,44 @@
+
+public class Calcula {
+	
+	public void somar(double num1, double num2) {
+		double result;
+		
+		result = num1 + num2;
+		
+		System.out.println(result);
+	}
+	
+	public void subtrair(double num1, double num2) {
+		double result;
+		
+		result = num1 - num2;
+		
+		System.out.println(result);
+	}
+	
+	public void multiplicar(double num1, double num2) {
+		double result;
+		
+		result = num1 * num2;
+		
+		System.out.println(result);
+	}
+	
+	public void divisao(double num1, double num2) {
+		double result;
+		
+		result = num1 / num2;
+		
+		System.out.println(result);
+	}
+	
+	public void restoDivisao(double num1, double num2) {
+		double result;
+		
+		result = num1 % num2;
+		
+		System.out.println(result);
+	}
+
+}
