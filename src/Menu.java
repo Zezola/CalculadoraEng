ee55af4da727053737a094b1268d4394f40fdf4d
Ronald <ronald.jc@hotmail.com>
2021-04-15 15:07:53 -0300

    Foi desenhado o menu

--- src/Menu.java
+++ src/Menu.java
@@ -7,18 +7,19 @@ public class Menu {
 		Scanner in = new Scanner(System.in);
 		Pausar pausar = new Pausar();
 		LimparTela limparTela = new LimparTela();
-		
-		System.out.println("0 - Sair");
-		System.out.println("1- Somar \t\t\t  2- Subtrair");
-		System.out.println("3- Multiplicar \t\t\t  4- Dividir");
-		System.out.println("5- Resto da divisão \t\t  6- Eleva ao quadrado");
-		System.out.println("7- Eleva ao cubo \t\t  8- Exponenciaçao");
-		System.out.println("9- Exponenciacao na base 10 \t 10- Logaritmo na base 10");
-		System.out.println("11- Logaritmo natural (base e) \t 12- Fatorial");
-		System.out.println("13- Media Aritmetica \t\t 14- Seno");
-		System.out.println("15- Cosseno \t\t\t 16- Tangente");
-		System.out.println("17- Equacao do primeiro grau \t 18- Equacao do segundo grau");
-		System.out.print("\nEscolha uma das opções: ");
+		System.out.println("+-------------------------------------------------------------+");
+		System.out.println("| 0 - Sair \t\t\t\t\t\t      |");
+		System.out.println("| 1- Somar  \t\t\t\t\t  2- Subtrair |");
+		System.out.println("| 3- Multiplicar \t\t\t\t   4- Dividir |");
+		System.out.println("| 5- Resto da divisão \t\t\t 6- Eleva ao quadrado |");
+		System.out.println("| 7- Eleva ao cubo \t\t\t     8- Exponenciaçao |");
+		System.out.println("| 9- Exponenciacao na base 10  \t     10- Logaritmo na base 10 |");
+		System.out.println("| 11- Logaritmo natural (base e) \t\t 12- Fatorial |");
+		System.out.println("| 13- Media Aritmetica \t\t\t\t     14- Seno |");
+		System.out.println("| 15- Cosseno \t\t\t\t\t 16- Tangente |");
+		System.out.println("| 17- Equacao do primeiro grau \t  18- Equacao do segundo grau |");
+		System.out.println("+-------------------------------------------------------------+");
+		System.out.print("\n \t\t     Escolha uma das opções:  ");
 				
 		opcao = in.nextShort();
 		
